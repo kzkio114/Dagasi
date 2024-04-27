cf66b0918811215eb94243191465ee872462fd1b
伊藤 一希 <kzkio114@gmail.com>
2024-04-27 13:09:31 +0900

    reactを投入

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
-// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App'; // 仮のReactコンポーネントのパス
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<App />, document.body.appendChild(document.createElement('div')));
+});
