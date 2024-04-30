dbbb2f37700d4e41a0ddc81ba91c71f4ae42cfe7
伊藤 一希 <kzkio114@gmail.com>
2024-04-30 21:02:31 +0900

    非同期(turbo_stream)

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,15 @@
 class TopController < ApplicationController
 
   def index
+
+  end
+
+  def add_button
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.append("buttons", partial: "top/button")
+      end
+    end
   end
 
 end
