49c1be370a6ddb5e45ff99244b2e957651e05e40
伊藤 一希 <kzkio114@gmail.com>
2024-04-30 23:07:03 +0900

    非同期追記(turbo_stream)

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,13 +1,20 @@
 class TopController < ApplicationController
 
   def index
-
   end
 
   def add_button
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: turbo_stream.append("buttons", partial: "top/button")
+      end
+    end
+  end
+
+  def remove_buttons
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update("buttons", "")
       end
     end
   end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
 // Entry point for the build script in your package.json
+import "./turbo_customizations.js"
 import "@hotwired/turbo-rails"
 import "./controllers"
