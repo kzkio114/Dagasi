ebc63369c7ebcf517c0e432791ef17d7a55c0c64
伊藤 一希 <kzkio114@gmail.com>
2024-05-02 15:22:48 +0900

    楽天API画像取得成功

--- app/services/rakuten_service.rb
+++ app/services/rakuten_service.rb
@@ -1,16 +1,19 @@
 class RakutenService
-   # 楽天市場商品検索APIを使用して商品を検索する
-   def self.search_items(keyword)
-    # RakutenWebService::Ichiba::Item.search は楽天市場商品検索APIをラップしたメソッドです
-    results = RakutenWebService::Ichiba::Item.search(keyword: keyword)
+    # 楽天市場商品検索APIを使用して商品を検索する
+    def self.search_items(keyword)
+      # RakutenWebService::Ichiba::Item.search は楽天市場商品検索APIをラップしたメソッドです
+      results = RakutenWebService::Ichiba::Item.search(keyword: keyword)
 
-    # APIからのレスポンスをそのまま返すか、必要に応じて加工して返す
-    results.map do |item|
+      # APIからのレスポンスをそのまま返すか、必要に応じて加工して返す
+      results.map do |item|
+        # 商品画像がある場合は最初の画像URLを使用し、ない場合はデフォルト画像URLを使用
+        image_url = item.image_flag == 1 && item.medium_image_urls.any? ? item.medium_image_urls.first : 'path/to/default-image.png'
+
         {
-          name: item['itemName'],
-          price: item['itemPrice'],
-          url: item['itemUrl'],
-          image_url: item['mediumImageUrl']
+          name: item.name,  # 商品名
+          price: item.price,  # 価格
+          url: item.affiliate_url,  # 商品のアフィリエイトURL
+          image_url: image_url  # 商品画像URL
         }
     end
   end
