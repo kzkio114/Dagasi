d41ae9a1e38feed0e7fc52ac5ac1e0779c6faf05
伊藤 一希 <kzkio114@gmail.com>
2024-05-06 13:47:33 +0900

    css修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,19 @@ module.exports = {
     "./app/javascript/**/*.{js,jsx,ts,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'nostalgic-turquoise': '#00a497',
+        'nostalgic-green': '#669977',// 懐かしい感じの緑色 colorsも追記
+        'vintage-blue': '#4a5d70', // 深いが暗めの青色
+        'cream': '#f5f5dc',
+        'soft-black': '#323232',
+        'vintage-red': '#cb4154',
+        'dark-vintage-red': '#b4001e'
+      }
+    },
   },
   plugins: [
-    require('daisyui'),
+    require('daisyui'), // DaisyUIプラグインを追加
   ],
 }
